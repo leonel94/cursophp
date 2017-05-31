7ba578e4f566b81996080a69a8b1a8e72d5eba3c
leonel <leo.bjj@gmail.com>
2017-05-31 15:57:56 -0600

    este es el codigo de break statment

---
+++ Modulo5/breakstatment.php
@@ -0,0 +1,15 @@
+<?php
+$x=1;
+switch ($x) {
+  case 1:
+    echo "One";
+  case 2:
+    echo "Two";
+  case 3:
+    echo "Three";
+  default:
+    echo "No match";
+}
+
+//Outputs "OneTwoThreeNo match"
+?>
