a6a688eb4d4365eabb19e0ec84660bf4dd344ff0
leonel <leo.bjj@gmail.com>
2017-05-31 16:33:05 -0600

    este es el codigo de form

---
+++ Modulo7/form.php
@@ -0,0 +1,7 @@
+<?php
+<form action="first.php" method="post">
+  <p>Name: <input type="text" name="name" /></p>
+  <p>Age: <input type="text" name="age" /></p>
+  <p><input type="submit" name="submit" value="Submit" /></p>
+</form>
+?>
