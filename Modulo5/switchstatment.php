90ca355c40a049764b2a235582b980f8438dc836
leonel <leo.bjj@gmail.com>
2017-05-31 15:53:43 -0600

    este es el codigo de switchstatment

---
+++ Modulo5/switchstatment.php
@@ -0,0 +1,13 @@
+<?php
+switch (n) {
+  case value1:
+    //code to be executed if n=value1
+    break;
+  case value2:
+     //code to be executed if n=value2
+     break;
+  ...
+  default:
+    // code to be executed if n is different from all labels
+}
+?>
