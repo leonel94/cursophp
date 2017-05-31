17787207e93cd30cafc191c9d8fb2f48e96a9d06
leonel <leo.bjj@gmail.com>
2017-05-31 16:50:06 -0600

    este es el codigo de writetofile

---
+++ Modulo8/writetofile.php
@@ -0,0 +1,17 @@
+<?php
+<?php
+$myfile = fopen("names.txt", "w");
+
+$txt = "John\n";
+fwrite($myfile, $txt);
+$txt = "David\n";
+fwrite($myfile, $txt);
+
+fclose($myfile);
+
+/* File contains:
+John
+David
+*/
+?>
+?>
