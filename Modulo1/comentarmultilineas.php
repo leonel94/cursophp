5ad96065a57b9c7c25ef92c0f98c4c1ed2c4b914
leonel <leo.bjj@gmail.com>
2017-05-31 14:32:11 -0600

    este es el codigo para comentarmultilineas

---
+++ Modulo1/comentarmultilineas.php
@@ -0,0 +1,12 @@
+<?php
+<?php   
+  echo "<p>Hello World!</p>";
+   /*
+   This is a multi-line comment block
+   that spans over
+   multiple lines
+   */
+   echo "<p>I am learning PHP!</p>";
+   echo "<p>This is my first program!</p>";
+?>
+?>
